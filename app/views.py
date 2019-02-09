5d43e08672ac6945a81d6e6500e0a359c01c0b68
Fahari <karonjekevin67@gmail.com>
2019-02-09 16:18:50 +0300

    use macros to make our code DRY. and allow for template inheritance

--- app/views.py
+++ app/views.py
@@ -15,4 +15,4 @@ def index():
     upcoming_movie = get_movies('upcoming')
     now_showing_movie = get_movies('now_playing')
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie 
+    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie)
