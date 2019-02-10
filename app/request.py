ec8abea987e3d5ab63a5ab414561c70c7a88764d
Fahari <karonjekevin67@gmail.com>
2019-02-10 14:46:00 +0300

    update our imports by importing the Movie class from the new _app/models.py_ file we have just created

--- app/request.py
+++ app/request.py
@@ -1,11 +1,21 @@
 from app import app
 import urllib.request,json
-from .models import movie
+from .models import Movie
 # Getting api key
-api_key = app.config['MOVIE_API_KEY']
+# api_key = app.config['MOVIE_API_KEY']
+# # Getting the movie base url
+# base_url = app.config["MOVIE_API_BASE_URL"]
+# Movie = movie.Movie
+
+# Getting api key
+api_key = None
 # Getting the movie base url
-base_url = app.config["MOVIE_API_BASE_URL"]
-Movie = movie.Movie
+base_url = None
+
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.config['MOVIE_API_KEY']
+    base_url = app.config['MOVIE_API_BASE_URL']
 
 def get_movies(category):
     '''
