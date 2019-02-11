3d490dd14d0f5f36ac0310e2c09f1a055a7c0d7d
Fahari <karonjekevin67@gmail.com>
2019-02-11 14:16:14 +0300

    import Migrate class and the MigrateCommand class.

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 from app import create_app,db
 from app.models import User,Role
 from flask_script import Manager,Server
+from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
 app = create_app('development')
