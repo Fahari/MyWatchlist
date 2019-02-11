aa53bf06ee3ec9f147806e8db33e68615a07a65d
Fahari <karonjekevin67@gmail.com>
2019-02-11 16:24:55 +0300

    install the flask-login extension and we can Initialize it inside our create_app function

--- app/models.py
+++ app/models.py
@@ -53,17 +53,17 @@ class User(db.Model):
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     pass_secure = db.Column(db.String(255))
 
-        @property
-        def password(self):
-            raise AttributeError('You cannot read the password attribute')
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
 
-        @password.setter
-        def password(self, password):
-            self.pass_secure = generate_password_hash(password)
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
 
 
-        def verify_password(self,password):
-            return check_password_hash(self.pass_secure,password)
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
 class Role(db.Model):
     __tablename__ = 'roles'
