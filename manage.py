bdac1276bece74baec7d11d0812383f0af82d80b
Fahari <karonjekevin67@gmail.com>
2019-02-11 05:33:57 +0300

    import db instance and user class. use the @manage.shell decorator to create a shell context

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 # from app import app
-from app import create_app
+from app import create_app,db
+from app.models import User
 from flask_script import Manager,Server
 
 # Creating app instance
@@ -15,6 +16,10 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
 
 if __name__ == '__main__':
     manager.run()
