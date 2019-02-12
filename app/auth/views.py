74b59b17a74e1a9744d68a0ac21d0f46f427822e
Fahari <karonjekevin67@gmail.com>
2019-02-12 05:03:55 +0300

    create an authenticated route logout that calls the flask_login's logout_userfunction.

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,7 @@
 from flask import render_template,redirect,url_for,flash,request
 from . import auth
 from .. import db
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 from ..models import User
 from .forms import LoginForm,RegistrationForm
 
@@ -33,3 +33,9 @@ def login():
 
     title = "watchlist login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
